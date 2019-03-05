fc8b87b39996633e373e99cd3212c58aec2de18a
janmichaelcabrera <janmichael.cabrera@gmail.com>
2019-03-05 17:36:09 -0600

    optimum brute forced

--- exercises/03/local_linear_smoother.py
+++ exercises/03/local_linear_smoother.py
@@ -5,15 +5,13 @@ import sys
 sys.path.append('../../scripts/')
 from smoothers import kernel_smoother
 import pandas as pd
-import seaborn as sns
 
 data = pd.read_csv('../../data/utilities.csv', delimiter=',')
 
 X = data['temp']
 # Y = data['gasbill']/data['billingdays']
-Y = data['gasbill']
-B = data['billingdays']
-Y = Y/B
+# Y = data['gasbill']
+Y = data['average']
 
 # sorted_data = data.sort_values('temp')
 # sorted_X = sorted_data['temp']
@@ -26,35 +24,49 @@ model.local_linear()
 # h = model.LOOCV()
 residuals = model.Residuals()
 
+
+
+h = np.logspace(0, 1, num=30)
+loocv = np.zeros(len(h))
+
+# print(h)
+for i in range(len(h)):
+    loocv[i] = model.LOOCV(x=h[i])
+
+n=0
+
+for i in range(len(h)):
+    if loocv[i] == loocv.min():
+        n=i
+# print(loocv.min())
+
+print(loocv)
+plt.figure()
+plt.plot(h, loocv)
+plt.show()
+plt.close()
+
+h = h[n]
+print(h)
+optimal = kernel_smoother(X, Y, x_star, h=h)
+optimal.local_linear()
+model = kernel_smoother(X, Y, X, h=h)
+model.local_linear()
+
+residuals = model.Residuals()
+
 plt.figure()
 plt.plot(X, residuals, '.k')
 plt.show()
 plt.close()
-# h = np.linspace(0.5, 10, num=20)
-# loocv = np.zeros(len(h))
-
-# # print(h)
-# for i in range(len(h)):
-#     loocv[i] = model.LOOCV(x=h[i])
-
-# print(loocv)
-# plt.figure()
-# plt.plot(h, loocv)
-# plt.show()
-# plt.close()
-
-# h = 5.38666509
-# print(h)
-# optimal = kernel_smoother(X, Y, x_star, h=h)
-# optimal.local_linear()
 
 # # plt.figure()
 # # plt.plot(sorted_X, residuals, '.k')
 # # plt.show()
 # # plt.close()
 
-# plt.figure()
-# plt.plot(X, Y, '.k')
-# plt.plot(x_star, optimal.y_star)
-# plt.show()
-# plt.close()
+plt.figure()
+plt.plot(X, Y, '.k')
+plt.plot(x_star, optimal.y_star)
+plt.show()
+plt.close()
