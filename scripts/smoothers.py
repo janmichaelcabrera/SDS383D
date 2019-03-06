fdd22912db701b0297202c59ae5522265368fe93
janmichaelcabrera <janmichael.cabrera@gmail.com>
2019-03-06 10:02:12 -0600

    smoothers updated with general case and other methods updated to work with general case

--- scripts/smoothers.py
+++ scripts/smoothers.py
@@ -48,7 +48,7 @@ class kernel_smoother:
     """
     This class returns a vector of smoothed values given feature and response vectors
     """
-    def __init__(self, x, y, x_star, h=1):
+    def __init__(self, x, y, x_star, kernel='gaussian', h=1, D=1):
         """
         Parameters
         ----------
@@ -68,46 +68,10 @@ class kernel_smoother:
         self.y = y
         self.x_star = x_star
         self.h = h
+        self.D = D
+        self.kernel=kernel
 
-    def local_constant(self,kernel='gaussian'):
-        """
-        Parameters
-        ----------
-            kernel: str (optional)
-                Kernel type to be used: Available kernels are uniform, gaussian, 
-
-        Returns
-        ----------
-            y_star: float, len(x_star)
-                Predictor for x_star
-                .. math:: y_i^* = \\sum_{i=1}^n w(x_i, x^*) y_i
-                .. math:: w(x_i, x^*) = \\frac{1}{2}K( \\frac{x_i - x^*}{h}), \\sum_{i=1}^n w(x_i, x^*) = 1
-        """
-        # Instantiate y_star
-        self.y_star = np.zeros(self.x_star.shape)
-
-        # Iterate through each value in y_star
-        for i in range(self.y_star.shape[0]):
-            
-            # Instantiate a weights vector
-            weights = np.zeros(self.x.shape)
-            
-            # Iterates through feature/response vectors
-            for j in range(self.x.shape[0]):
-
-                # X = \frac{(x_i - x^*)}{h}
-                X = (self.x[j] - self.x_star[i])/self.h
-
-                # w(x_i, x^*) = \frac{1}{h}K(X)
-                weights[j] = 1/self.h*getattr(kernels, kernel)(X)
-
-            # Normalizes weights such that \sum_{i=1}^n w(x_i, x^*) = 1    
-            weights = weights/weights.sum()
-
-            # y_i^* = \sum_{i=1}^n w(x_i, x^*) y_i
-            self.y_star[i] = (weights*self.y).sum()
-
-    def local_linear(self, kernel='gaussian'):
+    def local_general(self):
         """
         Parameters
         ----------
@@ -126,59 +90,7 @@ class kernel_smoother:
         self.Hat_matrix = []
 
         # Vector for determining y_star
-        e_1 = np.array([1, 0])
-
-        # Iterate through each value in y_star
-        for i in range(self.y_star.shape[0]):
-            
-            # Instantiate a weights vector
-            weights = np.zeros(self.x.shape)
-
-            # Instantiates X matrix 
-            X = np.transpose(np.array([np.ones(self.x.shape[0]), (self.x - self.x_star[i])]))
-            print(X.shape)
-            
-            # Iterates through feature/response vectors
-            for j in range(self.x.shape[0]):
-                weights[j] = 1/self.h * getattr(kernels, kernel)((self.x[j] - self.x_star[i])/self.h)
-
-            # Normalizes weights such that \sum_{i=1}^n w(x_i, x^*) = 1 
-            weights = weights/weights.sum()
-
-            # Construct weights matrix
-            W = np.diag(weights)
-
-            # Calculate hat matrix; H = (X^T W X)^{-1} X^T W
-            H = (inv(np.transpose(X) @ W @ X) @ np.transpose(X) @ W)
-
-            # Append hat matrix with current H matrix
-            self.Hat_matrix.append(e_1 @ H)
-
-            # \hat{f}(x^*) = e_1 H y
-            self.y_star[i] = e_1 @ H @ self.y
-
-        return self.y_star
-
-    def local_general(self, kernel='gaussian', D=1):
-        """
-        Parameters
-        ----------
-            kernel: str (optional)
-                Kernel type to be used: Available kernels are gaussian, 
-
-        Returns
-        ----------
-            y_star: float, len(x_star)
-                Predictor for x_star
-                .. math:: y_i^* = [1 0] H_i y
-                .. math:: w(x_i, x^*) = \\frac{1}{2}K( \\frac{x_i - x^*}{h}), \\sum_{i=1}^n w(x_i, x^*) = 1
-        """
-        # Instantiate y_star
-        self.y_star = np.zeros(self.x_star.shape)
-        self.Hat_matrix = []
-
-        # Vector for determining y_star
-        e_1 = np.append(1, np.zeros(D))
+        e_1 = np.append(1, np.zeros(self.D))
 
         # Iterate through each value in y_star
         for i in range(self.y_star.shape[0]):
@@ -188,14 +100,14 @@ class kernel_smoother:
 
             # Instantiates X matrix 
             # X = np.transpose(np.array([np.ones(self.x.shape[0]), (self.x - self.x_star[i])]))
-            X = np.transpose(np.ones((self.x.shape[0], D+1)))
+            X = np.transpose(np.ones((self.x.shape[0], self.D+1)))
 
-            for d in range(D):
+            for d in range(self.D):
                 X[d+1] = (self.x - self.x_star[i])**(d+1)
             X = np.transpose(X)
             # Iterates through feature/response vectors
             for j in range(self.x.shape[0]):
-                weights[j] = 1/self.h * getattr(kernels, kernel)((self.x[j] - self.x_star[i])/self.h)
+                weights[j] = 1/self.h * getattr(kernels, self.kernel)((self.x[j] - self.x_star[i])/self.h)
 
             # Normalizes weights such that \sum_{i=1}^n w(x_i, x^*) = 1 
             weights = weights/weights.sum()
@@ -205,7 +117,7 @@ class kernel_smoother:
 
             # Calculate hat matrix; H = (X^T W X)^{-1} X^T W
             H = (inv(np.transpose(X) @ W @ X) @ np.transpose(X) @ W)
-
+            print(H)
             # Append hat matrix with current H matrix
             self.Hat_matrix.append(e_1 @ H)
 
@@ -230,7 +142,7 @@ class kernel_smoother:
         self.y_test = y_test
         return np.mean((y_test - self.y_star)**2)
 
-    def MSE_optimization(self, y_test, smoother='local_constant'):
+    def MSE_optimization(self, y_test):
         """
         Parameters
         ----------
@@ -244,27 +156,27 @@ class kernel_smoother:
         """
         self.y_test = y_test
         def func(x):
-            Y = kernel_smoother(self.x, self.y, self.x_star, h=x)
-            getattr(Y, smoother)()
+            Y = kernel_smoother(self.x, self.y, self.x_star, kernel=self.kernel, h=x, D=self.D)
+            Y.local_general()
             return Y.MSE(self.y_test)
-        h_star = minimize(func, self.h)
+        h_star = minimize(func, 1)
         self.h = h_star.x
         return h_star.x
 
-    def LOOCV_optimization(self):
-        def func(x):
-            Y = kernel_smoother(self.x, self.y, self.x, h=x)
-            Y.local_linear()
-            return Y.LOOCV()
-        h_star = minimize(func, self.h)
-        self.h = h_star.x
-        return self.h
-
-    def LOOCV(self, smoother='local_linear'):
+    def LOOCV(self):
         loocv = np.zeros(len(self.y))
         for i in range(len(loocv)):
             loocv[i] = ((self.y[i] - self.y_star[i])/(1 - self.Hat_matrix[i][i]))**2
         return loocv.sum()
+
+    def LOOCV_optimization(self):
+        def func(X):
+            Y = kernel_smoother(self.x, self.y, self.x_star, kernel=self.kernel, h=X, D=self.D)
+            Y.local_general()
+            return Y.LOOCV()
+        h_star = minimize(func, 0.7)
+        self.h = h_star.x
+        return self.h
 
     def residuals(self):
         return self.y - self.y_star
