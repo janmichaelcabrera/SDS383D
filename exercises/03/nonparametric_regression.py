b171bd54fb9893164f0fda58ff15e7908394f2a8
janmichael.cabrera@gmail.com <janmichael.cabrera@gmail.com>
2019-03-13 12:56:31 -0500

    gp stuff kind of done

--- exercises/03/nonparametric_regression.py
+++ exercises/03/nonparametric_regression.py
@@ -7,6 +7,7 @@ sys.path.append('../../scripts/')
 from gaussian_process import gaussian_process
 import pandas as pd
 
+# Initialize random seed
 np.random.seed(3)
 
 # Import data
@@ -14,26 +15,40 @@ data = pd.read_csv('../../data/utilities.csv', delimiter=',')
 
 # Parse data
 X = data['temp']
-Y = np.log(data['gasbill']/data['billingdays'])
+Y = data['gasbill']/data['billingdays']
 
 # Sort X data for plotting purposes
 x_star = X.drop_duplicates().sort_values().values
 
-# test value
-# x_star = np.array([x_star[int(np.round(x_star.shape[0]/2))]])
-
+# Set hyperparameters
 b = 10
 tau_1_squared = 5
 tau_2_squared = 10**-6
 
+# Pack hyperparameters for passing to model
 hyperparams = b, tau_1_squared, tau_2_squared
 
+# Create a guassian process object from data and prediction vector
 GP = gaussian_process(X, hyperparams, y=Y, x_star=x_star)
-sigma_hat = GP.approx_sigma()
-y_star = GP.smoother(sigma_hat = sigma_hat)
 
+# Run an iteration of the gaussian process to make an approximation for the sample variance
+var = GP.approx_var()
+
+# Run the GP smoother with the approximated variance
+y_star, variance = GP.smoother(variance = var)
+
+# Calculate credible interval
+upper = y_star + np.sqrt(variance)*1.96
+lower = y_star - np.sqrt(variance)*1.96
+
+# Plot fit and credible interval
 plt.figure()
 plt.plot(X, Y, '.k')
 plt.plot(x_star, y_star, '-b')
+plt.plot(x_star, upper, '-g')
+plt.plot(x_star, lower, '-g')
+plt.xlabel('temperature ($^{\circ}$F)')
+plt.ylabel('normalized gassbill')
 plt.show()
+# plt.savefig('figures/utilities_fit_gp.pdf')
 plt.close()
--- scripts/gaussian_process.py
+++ scripts/gaussian_process.py
@@ -28,6 +28,33 @@ class covariance_functions:
     def __init__(self):
         pass
 
+    def squared_exponential(x_1, x_2, hyperparams):
+        """
+        Parameters
+        ----------
+            x: float (vector)
+                Vector of points
+
+        Returns
+        ----------
+            C: float (matrix)
+                Returns a Matern (5,2) square covariance matrix of size(x)
+                .. math:: C_{SE}(x_1, x_2) = \\tau_1^2 e^{-\\frac{1}{2} (d/b)^2} + \\tau_2^2 \\delta(x_1, x_2)
+        """
+
+        # Unpack hypereparameters
+        b, tau_1_squared, tau_2_squared = hyperparams
+
+        # Initialize covariance matrix
+        C = np.zeros((x_1.shape[0], x_2.shape[0]))
+
+        # Evaluate (i,j) components of covariance matrix
+        for i in range(x_1.shape[0]):
+            for j in range(x_2.shape[0]):
+                d = np.abs(x_1[i] - x_2[j])
+                C[i][j] = tau_1_squared*np.exp(-(1/2)*(d/b)**2) + tau_2_squared*kronecker_delta(x_1[i], x_2[j])
+        return C
+
     def matern_52(x_1, x_2, hyperparams):
         """
         Parameters
@@ -67,20 +94,20 @@ class gaussian_process:
             x: float
                 Feature vector
 
-            y: float
+            hyperparams: float (tuple)
+                Hyperparameters for a Guassian process
+
+            y: float (Not required for generating random samples)
                 Response vector
 
-            x_star: float
+            x_star: float (Not required for generating random samples)
                 Scalar or vector to be evaluated
 
-            kernel: str (optional)
-                Kernel type to be used: Available kernels are gaussian,
-                
-            h: float (optional)
-                Bandwidth
+            cov: str (optional)
+                Covariance function to be used. Available functions are:
+                    Squared exponential = 'squared_exponential'
+                    Matern 5/2 = 'matern_52'
 
-            D: int (optional)
-                Order of polynomial smoother
         """
         self.x = x
         self.hyperparams = hyperparams
@@ -88,34 +115,105 @@ class gaussian_process:
         self.y = y
         self.x_star = x_star
 
-    def residuals(self):
-        return 0
+    def approx_var(self):
+        """
+        Returns
+        ----------
+            variance: float(scalar)
+                Approximates the variance for a given problem using residual sum of squared errors. The function runs a Gaussian Process once for the approximation
 
-    def approx_sigma(self):
-        C_xx_star = getattr(covariance_functions, self.cov)(self.x, self.x, self.hyperparams)
+        Attributes
+        ----------
+            residuals: float, len(y)
+                residuals of prediction from data
+                .. math:: r_i = y_i - \\hat{y}_i
+        """
+
+        # Evaluate covariance
         C_xx = getattr(covariance_functions, self.cov)(self.x, self.x, self.hyperparams)
-        sigma_hat = 1
-        weights = C_xx_star @ inv(C_xx + sigma_hat*np.eye(self.x.shape[0]))
 
+        # Assume initial estimate for variance
+        variance = 1
+
+        # Calculate weights
+        weights = C_xx @ inv(C_xx + variance*np.eye(self.x.shape[0]))
+
+        # Calculate predictions from GP
         y_star = np.transpose(weights) @ self.y
+
+        # Calculate residuals from the above prediction
         self.residuals = self.y - y_star
+
         # Residual sum of squared errors
         rss = (self.residuals**2).sum()
+
         # Approximate standard error of fit
         return rss/(len(y_star)-1)
 
-    def smoother(self, sigma_hat=[]):
-        if not sigma_hat:
-            sigma_hat = 1
-        C_xx_star = getattr(covariance_functions, self.cov)(self.x_star, self.x, self.hyperparams)
-        C_xx = getattr(covariance_functions, self.cov)(self.x, self.x, self.hyperparams)
-        weights = C_xx_star @ inv(C_xx + sigma_hat*np.eye(self.x.shape[0]))
+    def smoother(self, variance=[]):
+        """
+            Parameters
+            ----------
+            variance: float (scalar; optional)
+                If not specified, the sample variance is assumed to be one
 
+            Returns
+            ----------
+            y_star: float, len(x_star)
+                Predictor for x_star, 
+                .. math:: y^* = W^T y
+                .. math:: W = C(x^*, x) (C(x, x) + \\sigma^2 I)^{-1}
+
+            post_var: float, len(x_star)
+                Posterior variance given the observed data
+                .. math:: var[f(x^*)| y] = C(x^*, x^*) - C(x^*, x) ( C(x, x) + \\sigma^2 I)^{-1} C(x^*, x)^T
+        """
+        # If a sample variance is not spcified, assume it is 1
+        if not variance:
+            variance = 1
+
+        # Evaluate C(x^*, x)
+        C_x_star_x = getattr(covariance_functions, self.cov)(self.x_star, self.x, self.hyperparams)
+        # Evaluate C(x, x)
+        C_xx = getattr(covariance_functions, self.cov)(self.x, self.x, self.hyperparams)
+        # Evaluate C(x^*, x^*)
+        C_star_star = getattr(covariance_functions, self.cov)(self.x_star, self.x_star, self.hyperparams)
+
+        # Calculate weights matrix,  W = C(x^*, x) (C(x, x) + \\sigma^2 I)^{-1}
+        weights = C_x_star_x @ inv(C_xx + variance*np.eye(self.x.shape[0]))
+
+        # Calculate y_star, y^* = W^T y
         y_star = np.transpose(weights) @ self.y
 
-        return y_star
+        # Calculates posterior variance, var[f(x^*)| y] = C(x^*, x^*) - C(x^*, x) ( C(x, x) + \\sigma^2 I)^{-1} C(x^*, x)^T
+        post_var = np.diag(C_star_star - C_x_star_x @ inv(C_xx + variance*np.eye(self.x.shape[0])) @ np.transpose(C_x_star_x) )
+
+        return y_star, post_var
 
     def generate_random_samples(self, mean=[]):
+        """
+        Parameters
+        ----------
+            mean: float, vector
+                Mean vector for multivariate normal, should have same shape as self.x. If not specified, assumed to be mean zero multivariate normal.
+
+        Returns
+        ----------
+            fx: float, len(x)
+                Returns random samples from a multivariate with specified mean
+
+        Raises
+        ----------
+            ValueError
+                If mean vector shape and x shape do not match
+        """
+
+        if not mean:
+            mean = np.zeros(self.x.shape[0])
+
+        if mean.shape != self.x.shape[0]:
+            raise ValueError('Mean shape, %i  and x shape, %i do not match'%(mean.shape[0], self.x.shape[0]))
+
         covariance = getattr(covariance_functions, self.cov)(self.x, self.x, self.hyperparams)
-        self.fx = multivariate_normal.rvs(mean=mean, cov=covariance)
-        return self.fx
+        fx = multivariate_normal.rvs(mean=mean, cov=covariance)
+        return fx
