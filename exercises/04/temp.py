6d52ef9cb2b60b828590b4ac5d7ac568fabdc4e3
janmichaelcabrera <janmichael.cabrera@gmail.com>
2019-04-24 12:47:02 -0500

    temp file added

---
+++ exercises/04/temp.py
@@ -0,0 +1,28 @@
+from __future__ import division
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+sys.path.append('../../scripts/')
+# from samplers import Trace
+import scipy.stats as stats
+from numpy.linalg import inv
+
+burn=0
+
+beta_trace = np.load('traces/hierarchical_probit/beta_trace.npy')
+mu_trace = np.load('traces/hierarchical_probit/mu_trace.npy')
+
+mu_trace_mean = np.mean(mu_trace[burn:], axis=0)
+
+beta_trace_mean = np.mean(beta_trace[burn:], axis=0)
+
+# print(beta_trace_mean)
+
+plt.figure()
+plt.plot(mu_trace[burn:])
+plt.show()
+
+plt.figure()
+plt.plot(beta_trace[burn:,0])
+plt.show()
