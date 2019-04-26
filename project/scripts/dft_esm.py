1aae04003ea1358df4f8dcf308759f46449522be
janmichaelcabrera <janmichael.cabrera@gmail.com>
2019-04-26 14:53:54 -0500

    project thing started

--- project/scripts/dft_esm.py
+++ project/scripts/dft_esm.py
@@ -29,17 +29,17 @@ def rCp_c(x):
     y = -9.517E-08*x**4 + 3.309E-04*x**3 - 4.394E-01*x**2 + 3.211E+02*x + 8.151E+04
     return y
     
-def k_c(x, alpha=None, beta=None):
-    if (alpha or beta) == None:
+def k_c(x, alpha=None):
+    if (alpha) == None:
         # conductivity of cerablanket (W/mK)
         y = 7.36E-17*x**5 - 3.02E-13*x**4 + 4.87E-10*x**3 - 2.35E-07*x**2 + 1.43E-04*x + 3.11E-03
         # INSWOOL
         # y = -2E-10*x**3 + 6E-07*x**2 - 0.0002*x + 0.0755
     else:
-        y = alpha*x + beta
+        y = alpha
     return y
 
-def energy_storage(Tf, Tr, my_times, alpha=None, beta=None):
+def energy_storage(Tf, Tr, my_times, alpha=None):
 
     # Geometry
     L_p = 0.001905         #thickness of plate (m)
@@ -51,7 +51,7 @@ def energy_storage(Tf, Tr, my_times, alpha=None, beta=None):
 
     T_ins = (Tf + Tr)/2
 
-    q_net = rCp_s(Tf+273)*L_p*np.gradient(Tf, my_times) + k_c(T_ins+273, alpha=alpha, beta=beta)*(Tf-Tr)/l_i+rCp_c(T_ins+273)*l_i*np.gradient(T_ins, my_times)
+    q_net = rCp_s(Tf+273)*L_p*np.gradient(Tf, my_times) + k_c(T_ins+273, alpha=alpha)*(Tf-Tr)/l_i+rCp_c(T_ins+273)*l_i*np.gradient(T_ins, my_times)
 
     q_ref = (1 - absorp)*sigma*((Tf+273)**4 - (Tf[0]+273)**4)
     q_emit = epsilon*sigma*((Tf+273)**4 - (Tf[0]+273)**4) + epsilon*sigma*((Tr+273)**4 - (Tr[0]+273)**4)
--- project/scripts/dft_param_cal.py
+++ project/scripts/dft_param_cal.py
@@ -8,95 +8,83 @@ import os, sys, time
 import pandas as pd
 from dft_esm import energy_storage
 from scipy.signal import savgol_filter
-from smooth import gp_smooth
-import pymc3 as pm
+from scipy.optimize import minimize
 
-input_directory = '../calibration/redux/smoothed/5_kw_m2/'
+input_directory = '../data/smoothed/5_kw_m2/'
 in_file = '1903-02_05.csv', '1903-03_05.csv', '1903-04_05.csv', '1903-05_05.csv', '1903-06_05.csv', '1903-07_05.csv', '1903-08_05.csv', '1903-09_05.csv', '1903-10_05.csv', '1903-12_05.csv', '1903-13_05.csv', '1903-15_05.csv', '1903-16_05.csv', '1903-17_05.csv', '1903-18_05.csv', '1903-19_05.csv'
 
-data = []
-Tf = []
-Tr = []
+# Read in all data
+all_data = []
 
 for i, item in enumerate(in_file):
-    data.append(pd.read_csv(input_directory+item,index_col=None))
-    Tf.append(data[i].tc_1.values)
-    Tr.append(data[i].tc_2.values)
+    all_data.append(pd.read_csv(input_directory+item)[:420])
 
-time = data[0].time.values[:420]
-q = np.zeros(len(time))
-q[60:360] = 5
+# Set time array
+all_time = all_data[0].time
 
-num_data = len(data)
+### Take the mean of the temperatures across all the data
+# Front mean temperature
+Tfm = np.mean([all_data[i].tc_1 for i in range(len(all_data))], axis=0)
+# Rear mean temperature
+Trm = np.mean([all_data[i].tc_2 for i in range(len(all_data))], axis=0)
 
-alpha, beta = None, None
+dat_index = 0
+# Read in one file from list of data set, keep only first 420 seconds
+data = pd.read_csv(input_directory+in_file[dat_index])[:420]
 
+# Instantiate an array for the "assumed, observed" heat flux
+q_obs = np.zeros(len(data.time))
+# Set elements in array from minute 1 to minute 6 at 5 kW/m^2
+q_obs[60:360] =5
 
-q_obs = np.array([q for n in range(10)])
+# Wrapper function for minimizing parameter of interest, k
+def func(k, q_obs, time, T_f, T_r):
+    """
+    Inputs
+    ----------
+        k: scalar
+            thermal conductivity W/m-k
 
-Tfm = np.mean([Tf[n][:420] for n in range(num_data)], axis=0)
-Trm = np.mean([Tr[n][:420] for n in range(num_data)], axis=0)
+        q_obs: vector
+            observed heat flux kW/m^2
 
-with pm.Model() as model:
-    # Priors
-    alpha = pm.Uniform('alpha', lower=-1, upper=1)
-    beta = pm.Uniform('beta', lower=-5, upper=5)
-    sigma = pm.HalfNormal('sigma', sd=1000)
+        time: vector
+            experimental times
 
-    q_pred = energy_storage(Tfm, Trm, time, alpha=alpha, beta=beta)
+        T_f: vector
+            measured temperature on front of device
 
-    y_obs = pm.Normal('y_obs', mu=q_pred, sd=sigma, observed=q_obs)
+        T_r: vector
+            measured temperature at rear of device
 
-    trace = pm.sample(2000)
-    pm.traceplot(trace)
-    plt.show()
+    Returns
+    ----------
+        squared error loss
+            .. math: loss = \\sum_{time} (q_pred - q_obs)^2
+    """
+    q_pred = energy_storage(T_f, T_r, time, alpha=k)
+    return ((q_pred - q_obs)**2).sum()
 
-alpha_low = pm.hpd(trace['alpha'])[0]
-alpha_high = pm.hpd(trace['alpha'])[1]
+# Minimize loss function
+res = minimize(func, 0.01, args=(q_obs, data.time, data.tc_1, data.tc_2))
 
-beta_low = pm.hpd(trace['beta'])[0]
-beta_high = pm.hpd(trace['beta'])[1]
+all_res = minimize(func, 0.01, args=(q_obs, all_time, Tfm, Trm))
+# Optimized parameter
+k_hat = res.x
+all_k_hat = all_res.x
+print(k_hat, all_k_hat)
 
-alpha, beta = trace['alpha'].mean(), trace['beta'].mean()
-print(alpha, beta)
+# Evaluate model at optimized parameter
+q_hat = energy_storage(data.tc_1, data.tc_2, data.time, alpha=k_hat)
+all_q_hat = energy_storage(Tfm, Trm, all_time, alpha=all_k_hat)
 
-q_low = energy_storage(Tfm, Trm, time, alpha=alpha_low, beta=beta_low)
-q_mean = energy_storage(Tfm, Trm, time, alpha=alpha, beta=beta)
-q_high = energy_storage(Tfm, Trm, time, alpha=alpha_high, beta=beta_high)
-
-
-
+# Plot results
 plt.figure()
-plt.plot(time, q, '-g', label='Calibration Set Point')
-plt.plot(time, q_mean, '-b', label='Mean')
-plt.fill_between(x=time, y1=q_low, y2=q_high, alpha=0.4, color='b')
+plt.plot(data.time, q_obs, label='Observed')
+plt.plot(data.time, q_hat, label='Predicted '+str(dat_index))
+plt.plot(data.time, all_q_hat, label='Mean Predicted')
+plt.xlim((0,420))
 plt.xlabel('Time (s)')
 plt.ylabel('Heat Flux (kW/m$^2$)')
 plt.legend(loc=0)
 plt.show()
-
-
-
-# q_pred = np.array([energy_storage(Tf[n], Tr[n], data[n].time.values, alpha=alpha, beta=beta) for n in range(num_data)]).T
-
-# print(q_pred)
-
-# with pm.Model() as model:
-#     alpha = pm.Uniform('alpha', lower=-1, upper=1)
-#     beta = pm.Uniform('beta', lower=-5, upper=5)
-#     sigma = pm.HalfNormal('sigma', sd=1000)
-
-#     q_pred = np.array([energy_storage(Tf[0], Tr[0], data[0].time.values, alpha=alpha, beta=beta)])
-
-#     y_obs = pm.Normal('y_obs', mu=q_pred, sd=sigma, observed=q)
-
-#     trace = pm.sample(100)
-#     pm.traceplot(trace)
-#     plt.show()
-
-
-# plt.figure()
-# plt.plot(data[0].time, energy_storage(Tf[0], Tr[0], data[0].time))
-# plt.plot(data[1].time, energy_storage(Tf[1], Tr[1], data[1].time))
-# plt.plot(data[0].time, q)
-# plt.show()
