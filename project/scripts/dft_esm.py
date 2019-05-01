variance thing corrected

project/scripts/dft_esm.py
@@ -119,6 +119,10 @@ def energy_storage(Tf, Tr, my_times, alpha=None):
     # Calcs insulation temperature as average of front and back temperatures
     T_ins = (Tf + Tr)/2
 
+    # Film Temperatures
+    T_ff = (Tf + Tf[0])/2 # film temp on front plate
+    T_fr = (Tr + Tr[0])/2 # film temp on rear plate
+
     # Evalutes ESM at Tf and Tr
     q_net = rCp_s(Tf+273)*L_p*np.gradient(Tf, my_times) + k_c(T_ins+273, alpha=alpha)*(Tf-Tr)/l_i+rCp_c(T_ins+273)*l_i*np.gradient(T_ins, my_times)
 
@@ -127,8 +131,9 @@ def energy_storage(Tf, Tr, my_times, alpha=None):
     # Emitted heat flux loss
     q_emit = epsilon*sigma*((Tf+273)**4 - (Tf[0]+273)**4) + epsilon*sigma*((Tr+273)**4 - (Tr[0]+273)**4)
     # Convective heat flux loss
-    q_conv = h_p(T_ins+273)*(Tf - Tf[0]) + h_p(T_ins+273)*(Tr - Tr[0])
+    # T_f (T_f + T_inf)/2
+    q_conv = h_p(T_ff+273)*(Tf - Tf[0]) + h_p(T_fr+273)*(Tr - Tr[0])
     # Incident heat flux
-    q_inc = q_net + q_ref + q_emit + q_conv
+    q_inc = q_net/epsilon + q_emit/epsilon + q_conv/epsilon
 
     return q_inc/1000

project/scripts/dft_param_cal.py
@@ -45,7 +45,7 @@ DFT = Models(data.tc_1, data.tc_2, data.time, q_obs)
 # DFT_all = Models(Tfm, Trm, data.time, q_obs)
 
 # # Run model
-DFT.metropolis(0.1, 10000)
+# DFT.metropolis(0.1, 10000)
 
 burn = 1000
 
@@ -57,19 +57,20 @@ print('Thermal conductivity - mean: {:2.2f}, std: {:2.4f}'.format(np.mean(alpha_
 print('Variance - mean: {:2.2f}, std: {:2.4f}'.format(np.mean(sigma_trace), np.std(sigma_trace)))
 
 k_hat_mh = np.mean(alpha_trace)
-k_hat_lower = k_hat_mh - np.std(alpha_trace)*1.96
-k_hat_upper = k_hat_mh + np.std(alpha_trace)*1.96
+# k_hat_lower = k_hat_mh - np.std(alpha_trace)*1.96
+# k_hat_upper = k_hat_mh + np.std(alpha_trace)*1.96
 
 # Get MLE
 k_hat = DFT.mle(0.01)
 # # all_k_hat = DFT_all.mle(0.01)
 
 # # Evaluate model at optimized parameter
-q_hat = energy_storage(data.tc_1, data.tc_2, data.time, alpha=k_hat)
+q_hat_mle = energy_storage(data.tc_1, data.tc_2, data.time, alpha=k_hat)
+
 # all_q_hat = energy_storage(Tfm, Trm, all_time, alpha=all_k_hat)
 q_hat_mh = energy_storage(data.tc_1, data.tc_2, data.time, alpha=k_hat_mh)
-q_hat_lower = energy_storage(data.tc_1, data.tc_2, data.time, alpha=k_hat_lower)
-q_hat_upper = energy_storage(data.tc_1, data.tc_2, data.time, alpha=k_hat_upper)
+q_hat_lower = q_hat_mh - np.sqrt(np.mean(sigma_trace))*1.96
+q_hat_upper = q_hat_mh + np.sqrt(np.mean(sigma_trace))*1.96
 
 plot = True
 if plot == True:
@@ -79,7 +80,7 @@ if plot == True:
     plt.plot(data.time, q_obs, '-k', linewidth=1.5, label='Observed')
     plt.plot(data.time, q_hat_mh, '-b', linewidth=1.5, label='MH '+str(dat_index))
     plt.plot(data.time, energy_storage(data.tc_1, data.tc_2, data.time), '-r', linewidth=1.5, label='Uncalibrated')
-    plt.plot(data.time, q_hat,'--g', linewidth=1.5, label='MLE '+str(dat_index))
+    plt.plot(data.time, q_hat_mle,'--g', linewidth=1.5, label='MLE '+str(dat_index))
     plt.xlim((0,420))
     plt.xlabel('Time (s)')
     plt.ylabel('Heat Flux (kW/m$^2$)')